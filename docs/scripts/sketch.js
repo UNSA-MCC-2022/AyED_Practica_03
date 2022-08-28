6d756a1b2bb02eee0ffb605a7af4b58055c6cabd
UNSA-MCC-2022 <rparedesro@unsa.edu.pe>
2022-08-28 14:40:47 -0500

    Add figures

--- docs/scripts/sketch.js
+++ docs/scripts/sketch.js
@@ -1,7 +1,27 @@
 let qt;
 let count = 0;
+
+// Dibujar tres (3) puntos [Punto 4]
+// function setup() {
+//     let quadCanvas = createCanvas(400, 400);
+//     quadCanvas.parent("QuadTreeCanvas");
+//     let boundary = new Rectangle(200, 200, 200, 200);
+//     qt = new QuadTree(boundary, 4);
+
+//     console.log(qt);
+//     for (let i = 0; i < 3; i++) {
+//         let p = new Point(Math.random() * 400, Math.random() * 400);
+//         qt.insert(p);
+//     }
+
+//     background(0);
+//     qt.show();
+// }
+
 function setup() {
-    createCanvas(400, 400);
+    let quadCanvas = createCanvas(400, 400);
+    quadCanvas.parent("QuadTreeCanvas");
+
     let boundary = new Rectangle(200, 200, 200, 200);
     qt = new QuadTree(boundary, 4);
 
@@ -15,25 +35,31 @@ function setup() {
     qt.show();
 }
 
+// Dibujar puntos con Mouse [Punto 6]
+// function draw() {
+//     background(0);
+//     if (mouseIsPressed) {
+//         for (let i = 0; i < 1; i++) {
+//             let m = new Point(mouseX + random(-5, 5), mouseY + random(-5,5));
+//             console.log(m);
+//             qt.insert(m);
+//         }
+//     }
+//     qt.show();
+// }
+
 function draw() {
-    /*background(0);
-    if (mouseIsPressed) {
-        for (let i = 0; i < 1; i++) {
-            let m = new Point(mouseX + random(-5, 5), mouseY + random(-5,5));
-            qt.insert(m);
-        }
-    }*/
     background(0);
     qt.show();
-    stroke(0,255,0);
-    rectMode ( CENTER );
-    let range = new Rectangle ( mouseX ,mouseY ,50 ,50);
-    rect ( range.x , range.y , range.w *2 , range.h *2) ;
+    stroke(0, 255, 0);
+    rectMode(CENTER);
+    let range = new Rectangle(mouseX, mouseY, 50, 50);
+    rect(range.x, range.y, range.w * 2, range.h * 2);
     //let points = [];
     //qt.query (range , points );
-    let points = qt.query (range );
-    for (let p of points ){
-        strokeWeight (6) ;
-        point (p.x , p.y );
+    let points = qt.query(range);
+    for (let p of points) {
+        strokeWeight(6);
+        point(p.x, p.y);
     }
 }
